4124df9fd13f9d9258607332a294345e443d30b8
Kiana LeDoux <kianaceleste@Kianas-MacBook-Air.local>
2019-10-14 10:31:39 -0400

    first commit

---
+++ Potter.js
@@ -0,0 +1,5 @@
+const baseURL = 'https://www.potterapi.com/v1/spells/'; // The URL which has the link - and defines the variable of the base URL
+const key = '$2a$10$DdC1bfLQHUl2UUyOy0kOVu7FxykGTb6/JDKsTi5d52t3apWRUXNyi'; //2
+let url; 
+
+const ul = document.getElementById('spells');
