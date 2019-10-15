99e013e1ba31c1d9b406382e14acb9b51f2329a2
Kiana LeDoux <kianaceleste@Kianas-MacBook-Air.local>
2019-10-15 10:35:53 -0400

    newfiles

--- Potter.js
+++ Potter.js
@@ -1,5 +1,43 @@
-const baseURL = 'https://www.potterapi.com/v1/spells/'; // The URL which has the link - and defines the variable of the base URL
-const key = '$2a$10$DdC1bfLQHUl2UUyOy0kOVu7FxykGTb6/JDKsTi5d52t3apWRUXNyi'; //2
-let url; 
+// API key
+const key = '$2a$10$DdC1bfLQHUl2UUyOy0kOVu7FxykGTb6/JDKsTi5d52t3apWRUXNyi';
 
-const ul = document.getElementById('spells');
+// create the list where we will place our characters in
+const ul = document.getElementById('ravenClaw');
+
+// create URL to get all Harry Potter characters
+const url = new URL('https://www.potterapi.com/v1/characters/'), params = {key: key}
+
+Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+// create the type of element on page as passed in the parameters
+function createNode(element) {
+  return document.createElement(element);
+}
+
+// Append the second parameter(element) to the first one
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+// make API request using Fetch API
+fetch(url, {key: key})
+  .then((resp) => resp.json()) // Transform the data into json
+  .then(function(data) {
+  // Get the results
+  let characters = data;
+  // console.log(characters);
+  //Map through the results
+  return characters.map(function(character) {
+    // Create the li's
+    let li = createNode('li');
+    // Make the HTML of our li the character's name and blood status
+    li.innerHTML = `${character.name}, ${character.house}`;
+    // Append the li's to the ul
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  // Error handling
+  let p = "There's an error with the API.";
+  append(body, p);
+});
