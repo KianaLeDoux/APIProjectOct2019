1ddc99a16fdb266f04aa3212b7ed8ffe174d26ca
Kiana LeDoux <kianaceleste@Kianas-MacBook-Air.local>
2019-10-16 23:30:26 -0400

    api

--- Potter.js
+++ Potter.js
@@ -1,43 +1,62 @@
-// API key
 const key = '$2a$10$DdC1bfLQHUl2UUyOy0kOVu7FxykGTb6/JDKsTi5d52t3apWRUXNyi';
-
-// create the list where we will place our characters in
-const ul = document.getElementById('ravenClaw');
-
-// create URL to get all Harry Potter characters
-const url = new URL('https://www.potterapi.com/v1/characters/'), params = {key: key}
+const ul = document.getElementById('characters');
+const url = new URL('https://www.potterapi.com/v1/characters/'), params = {key: key, house:'Gryffindor'}
+const characterTable = document.querySelector('form');
 
 Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
 
-// create the type of element on page as passed in the parameters
 function createNode(element) {
   return document.createElement(element);
 }
-
-// Append the second parameter(element) to the first one
 function append(parent, el) {
-  return parent.appendChild(el);
+    return parent.appendChild(el);
 }
-
-// make API request using Fetch API
 fetch(url, {key: key})
-  .then((resp) => resp.json()) // Transform the data into json
+  .then((resp) => resp.json()) 
   .then(function(data) {
-  // Get the results
+
   let characters = data;
-  // console.log(characters);
-  //Map through the results
-  return characters.map(function(character) {
-    // Create the li's
-    let li = createNode('li');
-    // Make the HTML of our li the character's name and blood status
-    li.innerHTML = `${character.name}, ${character.house}`;
-    // Append the li's to the ul
-    append(ul, li);
-  })
+  let charactersRow = document.createElement('tr');
+  let charactersName = document.createElement('td');
+  let charactersHouse = document.createElement('td');
+  let charactersSpecies = document.createElement('td');
+  let charactersRole = document.createElement('td');
+
+  	
+ 
+  
+  
+  //   let =  =false;
+  //   if (undefined == false) {
+      //       console.log('This character role is undefined');
+      //   }
+      // console.log(characters);
+      
+      charactersName.innerText = data.name;
+      charactersHouse.innerText = data.house;
+      charactersSpecies.innerText = data.species;
+      charactersRole.innerText = data.role;
+      
+      
+        
+        
+        return characters.map(function(characters) {
+            console.log(characters);
+            let li = createNode('li');
+            
+            li.innerText = `${characters.name}, ${characters.house}, ${characters.species}, ${characters.role}`;
+            append(ul, li);
+        
 })
-.catch(function(error) {
-  // Error handling
-  let p = "There's an error with the API.";
-  append(body, p);
-});
+})
+// characterTable.appendChild(characters);
+// characters.appendChild(charactersName);
+// characters.appendChild(charactersHouse);
+// characters.appendChild(charactersSpecies);
+// characters.appendChild(charactersRole);
+
+
+
+
+
+
